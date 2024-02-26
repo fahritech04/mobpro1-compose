619b590996f0e2ac728d29cb40eaf05d82d4524c
fahritech04 <fahricoding@gmail.com>
2024-02-26 09:44:03 +0700

    Testing

--- app/src/main/java/org/d3if3009/mobpro1/MainActivity.kt
+++ app/src/main/java/org/d3if3009/mobpro1/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(name = "Android")
+                    Greeting(name = "Muhammad Raihan Fahrifi")
                 }
             }
         }
